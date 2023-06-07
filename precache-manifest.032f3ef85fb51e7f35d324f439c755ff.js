84316388266a152f1eeeafc7ecfeda383cc4c102
Jack Eadie <jack@marqo.ai>
2023-06-07 21:12:38 +1000

    Updates

---
+++ precache-manifest.032f3ef85fb51e7f35d324f439c755ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbd875f1b614487bd00e",
+    "url": "/infima/static/css/main.4f8325fe.chunk.css"
+  },
+  {
+    "revision": "cbd875f1b614487bd00e",
+    "url": "/infima/static/js/main.cbd875f1.chunk.js"
+  },
+  {
+    "revision": "c703205ae00c061e669a",
+    "url": "/infima/static/js/1.c703205a.chunk.js"
+  },
+  {
+    "revision": "bcc12e4aa74eb7e83ce7",
+    "url": "/infima/static/js/runtime~main.bcc12e4a.js"
+  },
+  {
+    "revision": "79dffa94e9628dc3197ef85023255342",
+    "url": "/infima/index.html"
+  }
+];
